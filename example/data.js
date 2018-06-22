ba80e7f3aa744f83e671d152d0e049d96c9873da
Seoyoungb <40393202+Seoyoungb@users.noreply.github.com>
2018-06-23 00:20:40 +0900

    Update Data.js
    year 2018

--- example/data.js
+++ example/data.js
@@ -1,9 +1,9 @@
 var msbData = [
 	{
 		name: "Schedule 1", events: [
-			{ event: "Event 1", date: new Date(2010,00,01), stage: "Stage 1", color: "#f0f0f0" },
-			{ event: "Event 2", date: new Date(2010,00,02), stage: "Stage 2", color: "#f0f0f0" },
-			{ event: "Event 3", date: new Date(2010,00,04), color: "#f0f0f0" }
+			{ event: "Event 1", date: new Date(2018,00,01), stage: "Stage 1", color: "#f0f0f0" },
+			{ event: "Event 2", date: new Date(2018,00,02), stage: "Stage 2", color: "#f0f0f0" },
+			{ event: "Event 3", date: new Date(2018,00,04), color: "#f0f0f0" }
 		]
 	}
 ];
@@ -11,51 +11,51 @@ var msbData = [
 var ganttData = [
 	{
 		id: 1, name: "Feature 1", series: [
-			{ name: "Planned", start: new Date(2010,00,01), end: new Date(2010,00,03) },
-			{ name: "Actual", start: new Date(2010,00,02), end: new Date(2010,00,05), color: "#f0f0f0" }
+			{ name: "Planned", start: new Date(2018,00,01), end: new Date(2018,00,03) },
+			{ name: "Actual", start: new Date(2018,00,02), end: new Date(2018,00,05), color: "#f0f0f0" }
 		]
 	}, 
 	{
 		id: 2, name: "Feature 2", series: [
-			{ name: "Planned", start: new Date(2010,00,05), end: new Date(2010,00,20) },
-			{ name: "Actual", start: new Date(2010,00,06), end: new Date(2010,00,17), color: "#f0f0f0" },
-			{ name: "Projected", start: new Date(2010,00,06), end: new Date(2010,00,17), color: "#e0e0e0" }
+			{ name: "Planned", start: new Date(2018,00,05), end: new Date(2018,00,20) },
+			{ name: "Actual", start: new Date(2018,00,06), end: new Date(2018,00,17), color: "#f0f0f0" },
+			{ name: "Projected", start: new Date(2018,00,06), end: new Date(2018,00,17), color: "#e0e0e0" }
 		]
 	}, 
 	{
 		id: 3, name: "Feature 3", series: [
-			{ name: "Planned", start: new Date(2010,00,11), end: new Date(2010,01,03) },
-			{ name: "Actual", start: new Date(2010,00,15), end: new Date(2010,01,03), color: "#f0f0f0" }
+			{ name: "Planned", start: new Date(2018,00,11), end: new Date(2018,01,03) },
+			{ name: "Actual", start: new Date(2018,00,15), end: new Date(2018,01,03), color: "#f0f0f0" }
 		]
 	}, 
 	{
 		id: 4, name: "Feature 4", series: [
-			{ name: "Planned", start: new Date(2010,01,01), end: new Date(2010,01,03) },
-			{ name: "Actual", start: new Date(2010,01,01), end: new Date(2010,01,05), color: "#f0f0f0" }
+			{ name: "Planned", start: new Date(2018,01,01), end: new Date(2018,01,03) },
+			{ name: "Actual", start: new Date(2018,01,01), end: new Date(2018,01,05), color: "#f0f0f0" }
 		]
 	},
 	{
 		id: 5, name: "Feature 5", series: [
-			{ name: "Planned", start: new Date(2010,02,01), end: new Date(2010,03,20) },
-			{ name: "Actual", start: new Date(2010,02,01), end: new Date(2010,03,26), color: "#f0f0f0" }
+			{ name: "Planned", start: new Date(2018,02,01), end: new Date(2018,03,20) },
+			{ name: "Actual", start: new Date(2018,02,01), end: new Date(2018,03,26), color: "#f0f0f0" }
 		]
 	}, 
 	{
 		id: 6, name: "Feature 6", series: [
-			{ name: "Planned", start: new Date(2010,00,05), end: new Date(2010,00,20) },
-			{ name: "Actual", start: new Date(2010,00,06), end: new Date(2010,00,17), color: "#f0f0f0" },
-			{ name: "Projected", start: new Date(2010,00,06), end: new Date(2010,00,20), color: "#e0e0e0" }
+			{ name: "Planned", start: new Date(2018,00,05), end: new Date(2018,00,20) },
+			{ name: "Actual", start: new Date(2018,00,06), end: new Date(2018,00,17), color: "#f0f0f0" },
+			{ name: "Projected", start: new Date(2018,00,06), end: new Date(2018,00,20), color: "#e0e0e0" }
 		]
 	}, 
 	{
 		id: 7, name: "Feature 7", series: [
-			{ name: "Planned", start: new Date(2010,00,11), end: new Date(2010,01,03) }
+			{ name: "Planned", start: new Date(2018,00,11), end: new Date(2018,01,03) }
 		]
 	}, 
 	{
 		id: 8, name: "Feature 8", series: [
-			{ name: "Planned", start: new Date(2010,01,01), end: new Date(2010,01,03) },
-			{ name: "Actual", start: new Date(2010,01,01), end: new Date(2010,01,05), color: "#f0f0f0" }
+			{ name: "Planned", start: new Date(2018,01,01), end: new Date(2018,01,03) },
+			{ name: "Actual", start: new Date(2018,01,01), end: new Date(2018,01,05), color: "#f0f0f0" }
 		]
 	}
 ];
