27ac43c8476663f244249b6ea077ecc154a82121
Seoyoungb <40393202+Seoyoungb@users.noreply.github.com>
2018-07-08 10:59:10 +0900

    15 rows data
    15 rows data

--- example/data.js
+++ example/data.js
@@ -44,59 +44,89 @@ var msbData = [
 	{
 		id: 5, name: "5", series: [
 			{ name: "info1", start: new Date(2018,05,01), end: new Date(2018,05,20) , color: "#8DA45C" },
+			{ name: "info2", start: new Date(2018,05,21), end: new Date(2018,06,02) , color: "#604A7B" } ,
+			{ name: "info3", start: new Date(2018,06,03), end: new Date(2018,06,05) , color: "#31859C" } ,
+			{ name: "info4", start: new Date(2018,06,06), end: new Date(2018,06,16) , color: "#E46C0A" } 
+		]
+	}, 
+	{
+		id: 6, name: "6", series: [
+			{ name: "info1", start: new Date(2018,05,01), end: new Date(2018,05,03) , color: "#8DA45C" },
+			{ name: "info2", start: new Date(2018,05,04), end: new Date(2018,05,07) , color: "#604A7B"  } ,
+			{ name: "info3", start: new Date(2018,05,08), end: new Date(2018,06,11) , color: "#31859C" } ,
+			{ name: "info4", start: new Date(2018,06,12), end: new Date(2018,06,26) , color: "#E46C0A" } 
+		]
+	}, 
+	{
+		id: 7, name: "7", series: [
+			{ name: "info1", start: new Date(2018,05,01), end: new Date(2018,05,20) , color: "#8DA45C" },
+			{ name: "info2", start: new Date(2018,05,21), end: new Date(2018,05,30) , color: "#604A7B" } ,
+			{ name: "info3", start: new Date(2018,05,31), end: new Date(2018,06,09) , color: "#31859C" } ,
+			{ name: "info4", start: new Date(2018,06,10), end: new Date(2018,07,04) , color: "#E46C0A" } 
+		]
+	}, 
+	{
+		id: 8, name: "8", series: [
+			{ name: "info1", start: new Date(2018,05,01), end: new Date(2018,05,13) , color: "#8DA45C" },
+			{ name: "info2", start: new Date(2018,05,14), end: new Date(2018,05,17) , color: "#604A7B" } ,
+			{ name: "info3", start: new Date(2018,05,18), end: new Date(2018,06,05) , color: "#31859C" } ,
+			{ name: "info4", start: new Date(2018,06,06), end: new Date(2018,06,26) , color: "#E46C0A" } 
+		]
+	}, 
+	{
+		id: 9, name: "9", series: [
+			{ name: "info1", start: new Date(2018,05,01), end: new Date(2018,05,13) , color: "#8DA45C" },
+			{ name: "info2", start: new Date(2018,05,14), end: new Date(2018,05,27) , color: "#604A7B" } ,
+			{ name: "info3", start: new Date(2018,05,28), end: new Date(2018,06,21) , color: "#31859C" } ,
+			{ name: "info4", start: new Date(2018,06,22), end: new Date(2018,06,26) , color: "#E46C0A" } 
+		]
+	},
+	{
+		id: 10, name: "10", series: [
+			{ name: "info1", start: new Date(2018,05,01), end: new Date(2018,05,20) , color: "#8DA45C" },
 			{ name: "info2", start: new Date(2018,05,21), end: new Date(2018,05,23) , color: "#604A7B" } ,
 			{ name: "info3", start: new Date(2018,05,24), end: new Date(2018,06,01) , color: "#31859C" } ,
 			{ name: "info4", start: new Date(2018,06,02), end: new Date(2018,06,16) , color: "#E46C0A" } 
 		]
 	}, 
 	{
-		id: 6, name: "6", series: [
-			{ name: "info", start: new Date(2018,06,05), end: new Date(2018,06,20) , color: "#8DA45C"},
-		]
-	}, 
-	{
-		id: 7, name: "7", series: [
-			{ name: "info", start: new Date(2018,06,11), end: new Date(2018,06,03) , color: "#8DA45C"}
-		]
-	}, 
-	{
-		id: 8, name: "8", series: [
-			{ name: "info", start: new Date(2018,07,01), end: new Date(2018,07,03) , color: "#8DA45C" }
-		]
-	},
-	{
-		id: 9, name: "9", series: [
-			{ name: "info", start: new Date(2018,06,01), end: new Date(2018,06,03) , color: "#8DA45C" }
-		]
-	}, 
-	{
-		id: 10, name: "10", series: [
-			{ name: "info", start: new Date(2018,06,05), end: new Date(2018,06,20) , color: "#8DA45C"}
-		]
-	}, 
-	{
 		id: 11, name: "11", series: [
-			{ name: "info", start: new Date(2018,06,11), end: new Date(2018,07,03) , color: "#8DA45C"}
+			{ name: "info1", start: new Date(2018,05,01), end: new Date(2018,05,03) , color: "#8DA45C" },
+			{ name: "info2", start: new Date(2018,05,04), end: new Date(2018,05,07) , color: "#604A7B"  } ,
+			{ name: "info3", start: new Date(2018,05,08), end: new Date(2018,06,11) , color: "#31859C" } ,
+			{ name: "info4", start: new Date(2018,06,12), end: new Date(2018,06,26) , color: "#E46C0A" } 
 		]
 	}, 
 	{
 		id: 12, name: "12", series: [
-			{ name: "info", start: new Date(2018,06,01), end: new Date(2018,06,03) , color: "#8DA45C"}
+			{ name: "info1", start: new Date(2018,05,01), end: new Date(2018,05,20) , color: "#8DA45C" },
+			{ name: "info2", start: new Date(2018,05,21), end: new Date(2018,05,30) , color: "#604A7B" } ,
+			{ name: "info3", start: new Date(2018,05,31), end: new Date(2018,06,09) , color: "#31859C" } ,
+			{ name: "info4", start: new Date(2018,06,10), end: new Date(2018,07,04) , color: "#E46C0A" } 
 		]
-	},
+	}, 
 	{
 		id: 13, name: "13", series: [
-			{ name: "info", start: new Date(2018,07,01), end: new Date(2018,07,20) , color: "#8DA45C"}
+			{ name: "info1", start: new Date(2018,05,01), end: new Date(2018,05,13) , color: "#8DA45C" },
+			{ name: "info2", start: new Date(2018,05,14), end: new Date(2018,05,17) , color: "#604A7B" } ,
+			{ name: "info3", start: new Date(2018,05,18), end: new Date(2018,06,05) , color: "#31859C" } ,
+			{ name: "info4", start: new Date(2018,06,06), end: new Date(2018,06,26) , color: "#E46C0A" } 
 		]
 	}, 
 	{
 		id: 14, name: "14", series: [
-			{ name: "info", start: new Date(2018,06,05), end: new Date(2018,06,20) , color: "#8DA45C"}
+			{ name: "info1", start: new Date(2018,05,01), end: new Date(2018,05,13) , color: "#8DA45C" },
+			{ name: "info2", start: new Date(2018,05,14), end: new Date(2018,05,27) , color: "#604A7B" } ,
+			{ name: "info3", start: new Date(2018,05,28), end: new Date(2018,06,21) , color: "#31859C" } ,
+			{ name: "info4", start: new Date(2018,06,22), end: new Date(2018,06,26) , color: "#E46C0A" } 
 		]
-	}, 
+	},
 	{
 		id: 15, name: "15", series: [
-			{ name: "info", start: new Date(2018,06,11), end: new Date(2018,07,03) , color: "#8DA45C"}
+			{ name: "info1", start: new Date(2018,05,01), end: new Date(2018,05,20) , color: "#8DA45C" },
+			{ name: "info2", start: new Date(2018,05,21), end: new Date(2018,05,23) , color: "#604A7B" } ,
+			{ name: "info3", start: new Date(2018,05,24), end: new Date(2018,06,01) , color: "#31859C" } ,
+			{ name: "info4", start: new Date(2018,06,02), end: new Date(2018,06,16) , color: "#E46C0A" } 
 		]
 	}, 
 	{
